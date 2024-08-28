bceaad57267a76537d641fba37b925a055cb9545
sumonpaul55 <sumpaul100@gmail.com>
2024-08-28 15:31:07 +0600

    user button with drop down completed

--- src/pages/CommonPages/Navbar.tsx
+++ src/pages/CommonPages/Navbar.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Menu, Button, Drawer, MenuProps } from "antd";
+import { Menu, Button, Drawer, MenuProps, Dropdown } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import logo from "../../assets/logo.png"
 import { navbarGenerator } from "../../utils/navbarGenerator";
 import { userPath } from "../../utils/userPaths";
+import { FaUser } from "react-icons/fa";
+import { BiBox, BiExit } from "react-icons/bi";
 type MenuItem = Required<MenuProps>['items'][number];
 
 
@@ -20,7 +22,18 @@ const Navbar = () => {
     };
 
     const items: MenuItem[] = navbarGenerator(userPath)
-    items.push({ key: "login", label: <NavLink to="/login">Login/SignUp</NavLink> })
+    items.push(
+        { key: "login", label: <NavLink to="/login">Login/SignUp</NavLink> }
+        // { key: "user", label: <FaUser /> }
+    )
+    const UserdropDownItems: MenuItem[] = [
+        {
+            key: "Logout", label: <Button className="px-2 w-full justify-start">Logout <BiExit size={16} /></Button>
+        },
+        {
+            key: "Logout", label: <NavLink to="/login"><Button className="px-2">My Bookings <BiBox size={16} /></Button></NavLink>
+        }
+    ]
     return (
         <nav className="bg-white shadow-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -33,7 +46,11 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className="hidden md:flex space-x-4 items-center w-full">
-                        <Menu mode="horizontal" style={{ width: "100%", justifyContent: "end", border: "0", fontSize: "18px", fontWeight: 500 }} items={items} />
+                        <Menu mode="horizontal" style={{ width: "100%", justifyContent: "end", border: "0", fontSize: "17px", fontWeight: 500 }} items={items} />
+                        {/* user icon and dropdown based on user */}
+                        <Dropdown trigger={['click']} menu={{ items: UserdropDownItems }} arrow={true} overlayStyle={{ border: "1px solid #ccc", borderRadius: "8px" }}>
+                            <Button className="px-6 font-semibold">User <FaUser size={15} /></Button>
+                        </Dropdown>
                     </div>
                     <div className="flex md:hidden items-center">
                         <Button type="primary" icon={<MenuOutlined />} onClick={showDrawer} />
@@ -46,6 +63,9 @@ const Navbar = () => {
                 onClose={closeDrawer}
                 open={visible}>
                 <Menu mode="inline" onClick={closeDrawer} items={items} />
+                <Dropdown className="ml-7" trigger={['click']} menu={{ items: UserdropDownItems }} arrow={true}>
+                    <Button className="px-7">User <FaUser size={15} /></Button>
+                </Dropdown>
             </Drawer>
         </nav>
     );
