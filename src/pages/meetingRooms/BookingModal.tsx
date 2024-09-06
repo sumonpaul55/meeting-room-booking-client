b49376d6182ff80aa9594dc8cc7c96fc57449a06
sumonpaul55 <sumpaul100@gmail.com>
2024-09-07 03:18:41 +0600

    dynamic date based on available slot and dynamic slot baseed on selected date

--- src/pages/meetingRooms/BookingModal.tsx
+++ src/pages/meetingRooms/BookingModal.tsx
@@ -1,23 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 import { Button, Flex, Modal } from 'antd';
-import { DatePicker, Select, Form, Input } from 'antd';
+import { DatePicker, Form, Input } from 'antd';
 import { useAppSelector } from '../../redux/hooks';
 import { selectCurrentUser } from '../../redux/features/auth/authSlice';
 import RoomForm from '../../components/forms/RoomForm';
 import RoomInputNumber from '../../components/forms/RoomNumberInput';
 import { TRoomData } from '../../types/roomtype';
 import { useGetOneUserQuery } from '../../redux/features/auth/auth.api';
-import RoomDatePicker from '../../components/forms/RoomDatePicker';
 import { useGetAllSlotsQuery } from '../../redux/features/roomManagement/slot.api';
 import dayjs from 'dayjs';
 import { Tsolts } from '../Dashboard/slots/slotType';
 import { RangePickerProps } from 'antd/es/date-picker';
+import RoomSelect from '../../components/forms/RoomSelelct';
+
+
 const BookingModal = ({ room }: { room: TRoomData }) => {
-
-
-
-    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedDate, setSelectedDate] = useState<any>()
     const [isModalOpen, setIsModalOpen] = useState(false);
     const localUser = useAppSelector(selectCurrentUser)
     const { data } = useGetOneUserQuery(localUser?.email)
@@ -33,30 +32,34 @@ const BookingModal = ({ room }: { room: TRoomData }) => {
     const handleCancel = () => {
         setIsModalOpen(false);
     };
-    const handleDateChange = (date: any) => {
-        setSelectedDate(date.format('YYYY-MM-DD'));
-    };
+
     const onSubmit = async (data) => {
         const bookingData = {
         }
     }
 
-
     const availableDates: any[] = []; // Dates to enable
+    const availableSlotsbySelectedDate: { label: string, value: string }[] = []
+    slots?.map((slots: Tsolts) => {
+        const formattedSelectedDate = dayjs(selectedDate).format('YYYY-MM-DD'); // Ensure consistent formatting
 
-    slots?.map((date: Tsolts) => {
-        const formattedDate = dayjs(date?.date).format('YYYY-MM-DD'); // Ensure consistent formatting
-        if (!availableDates.includes(formattedDate)) {
-            availableDates.push(formattedDate);
+        const formattedAvailableDate = dayjs(slots?.date).format('YYYY-MM-DD'); // Ensure consistent formatting
+        // console.log(formattedAvailableDate)
+        if (!availableDates.includes(formattedAvailableDate)) {
+            availableDates.push(formattedAvailableDate);
+        }
+        if (formattedSelectedDate === formattedAvailableDate && slots?.isBooked === false) {
+            availableSlotsbySelectedDate.push({
+                label: `${slots?.startTime} - ${slots?.endTime}`,
+                value: slots?._id
+            })
         }
     });
-
     const disabledDate: RangePickerProps['disabledDate'] = (current) => {
         const formattedCurrentDate = dayjs(current).format('YYYY-MM-DD'); // Format the current date to match availableDates
         // Disable all dates except for the availableDates and prevent selecting past dates
-        return current && (
-            current < dayjs().endOf('day') || !availableDates.includes(formattedCurrentDate)
-        );
+        return !availableDates.includes(formattedCurrentDate)
+
     };
 
     return (
@@ -75,12 +78,14 @@ const BookingModal = ({ room }: { room: TRoomData }) => {
                             <Input defaultValue={localUser?.email} disabled />
                         </Form.Item>
                     </Flex>
-
                     <Flex gap={5} justify='space-between' align='center'>
-                        <RoomDatePicker name="date" label="Available Date" disabledDate={disabledDate} />
+                        <Form.Item label="Available Date">
+                            <DatePicker disabledDate={disabledDate} onChange={(value) => setSelectedDate(value)} />
+                        </Form.Item>
 
                         <RoomInputNumber name='phone' className='w-full' label="Phone" defaultValue={user?.phone} />
                     </Flex>
+                    <RoomSelect mode='multiple' options={availableSlotsbySelectedDate} name='slots' label='Availalbe Slots' placeholder="Select Slots" disabled={!availableSlotsbySelectedDate.length} />
 
                 </RoomForm>
             </Modal>
