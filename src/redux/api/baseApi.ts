80107fa2565c989b1ef0a4ccbb9c6cb2c875722d
sumonpaul55 <sumpaul100@gmail.com>
2024-10-27 02:45:21 +0600

    update live link

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
-  // baseUrl: "https://room-booking-server2.vercel.app/api",
+  // baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://room-booking-server2.vercel.app/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
